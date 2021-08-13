3eaf303b3dba4555f32c70515ff54821e5ba0231
anop <anopsingh.rathore@vernost.in>
2021-08-13 18:59:21 +0530

    sign in

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SigninAndSignUp from './pages/sign-in-and-sign-up/signin-and-signup.component';
+
 function App() {
   return (
     <div>
@@ -10,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route  path="/shop" component={ShopPage}></Route>
+        <Route path="/signin" component={SigninAndSignUp}></Route>
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/signin-and-signup.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './signin-and-signup.component.style.scss';
+import SignIn from '../../components/signin/signin.component';
+const SigninAndSignUp = () => (
+    <div className='sign-in-and-sign-up'>
+     <SignIn/>
+    </div>
+)
+
+export default SigninAndSignUp;
